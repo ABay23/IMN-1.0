6b02f47ef4226487e13f166a6ff47bac3a02e698
ABay23 <alejandrobay23@gmail.com>
2023-02-17 19:56:08 -0500

    Create, delete and Update Tickets

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -4,7 +4,7 @@ const User = require('../models/userModels')
 
 const Ticket = require('../models/ticketModel')
 
-// Get user Ticket
+// Get user Tickets
 // @Route GET api/tickets
 // @Access Private
 const getTickets = asyncHandler(async (req, res) => {
@@ -18,18 +18,101 @@ const getTickets = asyncHandler(async (req, res) => {
 
   const tickets = await Ticket.find({ user: req.user.id })
   res.status(200).json(tickets)
+})
 
-  res.status(200).json({ message: 'Get Tickets' })
+// Get single Ticket
+// @Route GET api/tickets/:id
+// @Access Private
+const getTicket = asyncHandler(async (req, res) => {
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found ')
+  }
+
+  // Limiting tickets to only one user
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not Authorized')
+  }
+  res.status(200).json(ticket)
+})
+
+// Delete single Ticket
+// @Route DELETE api/tickets/:id
+// @Access Private
+const deleteTicket = asyncHandler(async (req, res) => {
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found ')
+  }
+
+  // Limiting tickets to only one user
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not Authorized')
+  }
+
+  await ticket.remove()
+
+  res.status(200).json({ success: true })
+})
+
+// Update Ticket
+// @Route PUT api/tickets/:id
+// @Access Private
+const updateTicket = asyncHandler(async (req, res) => {
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found ')
+  }
+
+  // Limiting tickets to only one user
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not Authorized')
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  )
+  res.status(200).json(updatedTicket)
 })
 
 // Create New Ticket
 // @Route POST api/tickets
 // @Access Private
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'createTickets' })
+  const { product, description } = req.body
+
+  if (!product || !description) {
+    res.status(400)
+    throw new Error('Please add a product and description')
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: 'new',
+  })
+
+  res.status(201).json(ticket)
 })
 
 module.exports = {
   getTickets,
+  getTicket,
   createTicket,
+  updateTicket,
+  deleteTicket,
 }
